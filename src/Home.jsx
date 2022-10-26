45d697371d179bc334bbe29c57d83cff0951e4ec
Iffat Hossain <iffathossain77@gmail.com>
2022-10-25 21:20:43 -0400

    updates to responsive design for form

--- src/Home.jsx
+++ src/Home.jsx
@@ -143,68 +143,61 @@ function Home() {
     return (
       <div className="App">
         <div className="initial-container h-screen w-screen bg-[#121827]">
-          <div className="initial-form-container h-full flex flex-col items-center">
-            <h1 className="mt-6 mb-28 block text-white text-base md:text-2xl font-bold">
+          <div className="initial-form-container  h-full flex flex-col items-center space-y-8">
+            <h1 className=" mt-20 mb-28 block text-white text-base md:text-2xl font-bold">
               Apex Stat Tracker
             </h1>
             <h2 className=" text-red-400">{err}</h2>
+            <div className="form-container w-9/12 md:w-1/2 lg:w-1/3 xl:w-1/4  ">
             <form
-              className="shadow-md rounded h-2/5 w-8/12 md:w-1/2 lg:w-1/4 px-4 lg:px-4 flex flex-col justify-evenly border dark:bg-gray-800 dark:border-gray-700"
+              className="shadow-md rounded-xl p-8 space-y-4 flex flex-col border justify-evenly dark:bg-gray-800 dark:border-gray-700"
               onSubmit={handleSubmit2}
             >
               <div className="input-wrapper">
-                <label
-                  htmlFor="role"
-                  className="block text-white text-sm font-bold mb-2"
-                >
-                  Platform
-                </label>
-                <select
-                  className="flex-shrink-0 z-10 inline-flex items-center w-full py-2 px-3
-                  text-sm font-medium text-center text-gray-900 border
-                   border-black rounded"
-                  name="platform"
-                  id="platform"
-                  onChange={handleChange2}
-                >
-                  <option defaultValue="Select Platform">
-                    Select Platform
-                  </option>
-                  <option value="xbl/">Xbox</option>
-                  <option value="psn/">Playstation</option>
-                  <option value="origin/">Origin</option>
-                </select>
-              </div>
-              <div className="input-wrapper">
-                <label
-                  htmlFor="enter-user"
-                  className="block text-white text-sm font-bold mb-2"
-                >
-                  Enter Username
-                </label>
-                <input
-                  required
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
-                  leading-tight text-sm font-medium text-center 
-                  focus:outline-none focus:shadow-outline"
-                  id="user"
-                  name="user"
-                  placeholder="Enter Username"
-                  type="search"
-                  onChange={handleChange2}
-                  autoComplete="on"
-                />
-              </div>
-              <button
-                className="search-btn text-white bg-blue-700 hover:bg-blue-800 
-              focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
-              rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center
-             dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="submit"
+              <label
+                htmlFor="role"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              />
+              <select
+                className="flex-shrink-0 z-10 inline-flex items-center w-full py-2 px-3
+                text-sm font-medium text-center text-gray-900 border
+                 border-black rounded"
+                name="platform"
+                id="platform"
+                onChange={handleChange2}
               >
-                Search for player
-              </button>
-            </form>
+                <option defaultValue="Select Platform">Select Platform</option>
+                <option value="xbl/">Xbox</option>
+                <option value="psn/">Playstation</option>
+                <option value="origin/">Origin</option>
+              </select>
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="enter-user" />
+              <input
+                required
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+                leading-tight text-sm font-medium text-center 
+                focus:outline-none focus:shadow-outline"
+                id="user"
+                name="user"
+                placeholder="Enter Username"
+                type="search"
+                onChange={handleChange2}
+                autoComplete="on"
+              />
+            </div>
+            <button
+              className="search-btn text-white bg-blue-700 hover:bg-blue-800 
+              font-medium
+              rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center
+             dark:bg-blue-600 dark:hover:bg-blue-700"
+              type="submit"
+            >
+              Search for player
+            </button>
+              </form>
+              </div>
           </div>
         </div>
       </div>
@@ -212,13 +205,14 @@ function Home() {
   }
   return (
     <div>
-      <div className=" h-screen w-screen overflow-scroll bg-[#121827]">
-        <div className="home-container overflow-y-scroll h-3/4 flex flex-col items-center space-y-8">
-          <h1 className="mt-8 block text-white text-base md:text-2xl font-bold">
+      <div className=" h-screen w-screen overflow-scroll bg-[#121827] flex flex-col space-y-10">
+          <h1 className="mt-8 block text-white text-base md:text-2xl font-bold text-center">
             Apex Stats Tracker
           </h1>
+        <div className="home-container h-full flex flex-col items-center justify-center space-y-8">
+          <div className="form-container w-9/12 md:w-1/2 lg:w-1/3 xl:w-1/4  ">
           <form
-            className="shadow-md rounded h-2/5 w-8/12 md:w-1/2 lg:w-1/4 px-4 lg:px-4 flex flex-col border justify-evenly dark:bg-gray-800 dark:border-gray-700"
+            className="shadow-md rounded-xl p-8 space-y-4 flex flex-col border justify-evenly dark:bg-gray-800 dark:border-gray-700"
             onSubmit={handleSubmit2}
           >
             <div className="input-wrapper">
@@ -264,12 +258,13 @@ function Home() {
             >
               Search for player
             </button>
-          </form>
+            </form>
+            </div>
         </div>
 
 
-        <div className="card-container grid gird-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  lg:gap-x-6 gap-x-4 lg:gap-y-2 gap-y-1  lg:col-span-3 overflow-scroll place-items-center">
-          <div className="w-9/12 lg:group-hover:scale-105 group-hover:shadow-me duration-300 rounded-xl square aspect-w-1 aspect-h-1 overflow-hidden bg-gray-700 shadow-me">
+        <div className="card-container grid gird-cols-2 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 lg:gap-x-6 gap-x-4 gap-y-4 lg:gap-y-2 lg:col-span-3 place-items-center">
+          <div className="w-9/12 h-full rounded-xl square aspect-w-1 aspect-h-1 bg-gray-700 shadow-me p-4">
             <div>
             <div className="img-container p-4 flex justify-center h-full w-full ">
               <img
@@ -288,15 +283,19 @@ function Home() {
               </div> 
           </div>
           </div>
-          {/* <div className="legend-card w-full relative fade-in lg:group-hover:scale-105 group-hover:shadow-me duration-300 rounded-xl square aspect-w-1 aspect-h-1 overflow-hidden bg-gray-100 shadow-me">
-            <h3 className="p-3 text-white text-base md:text-2xl font-bold">{`Current Legend: ${legendName}`}</h3>
-            <div className="img-container animate-flash-once
-            w-full h-full bg-white opacity-0 absolute z-50">
 
-              <img className="duration-300 w-full h-full object-center object-cover absolute" src={legendPic}></img>
+
+          <div className="legend-card w-9/12 h-full rounded-xl square aspect-w-1 aspect-h-1 bg-gray-700 shadow-me font-maven p-4">
+            <div className="flex flex-col items-center" >
+            <p className="p-3 text-white text-base md:text-lg lg:text-2xl font-bold ">{`Current Legend: ${legendName}`}</p>
+            <div className="img-container p-4 flex justify-center h-full w-full">
+              <img className="legend-card-img object-center object-contain rounded-xl h-4/5 w-4/5 " src={legendPic}></img>
+              </div>
             </div>
-          </div> */}
+          </div>
         </div>
+      <section className="footer h-1/6  ">
+      </section>
       </div>
     </div>
   );
