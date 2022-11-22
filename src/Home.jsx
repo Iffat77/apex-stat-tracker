5b9f29ef107453684d2b662e49dbe47306923a37
Iffat Hossain <iffathossain77@gmail.com>
2022-11-22 15:22:28 -0500

    minor style change

--- src/Home.jsx
+++ src/Home.jsx
@@ -142,7 +142,7 @@ function Home() {
   if (isLoading || isErr) {
     return (
       <div className="App">
-        <div className="initial-container h-screen w-screen bg-gradient-to-br from-gray-900 to-gray-600 bg-gradient-to-r ">
+        <div className="initial-container h-screen w-screen from-gray-900 to-gray-600 bg-gradient-to-r ">
           <div className="initial-form-container  h-full flex flex-col items-center space-y-8">
             <h1 className=" mt-20 mb-28 block text-white text-base md:text-2xl font-bold">
               Apex Stat Tracker
@@ -208,29 +208,29 @@ function Home() {
       {/* bg-gradient-to-br from-gray-900 to-gray-600 bg-gradient-to-r  */}
       {/* bg-gradient-to-tl from-cyan-900 via-green-600 to-green-500  */}
       <div className=" h-screen w-screen overflow-scroll flex flex-col space-y-28 
-      bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-green-200 via-gray-100 to-slate-200
+      from-gray-900 to-gray-600 bg-gradient-to-r
       ">
         <div className="pb-8 rounded-b-xl flex justify-center shadow-md bg-[]">
           <h1 className="mt-8 block text-white text-base md:text-2xl 
           font-semibold text-center border py-2 px-4
-          rounded-xl shadow-sm bg-[#84d36c]">
+          rounded-xl shadow-sm bg-[#6186ad]">
             Apex Stats Tracker
           </h1>
         </div>
         <div className="home-container h-full flex flex-col items-center justify-center space-y-8">
           <div className="form-container w-9/12 md:w-1/2 lg:w-1/3 xl:w-1/4  ">
           <form
-            className="shadow-md rounded-xl p-8 space-y-4 flex flex-col border justify-evenly bg-[#eff4f2]"
+            className="shadow-md rounded-xl p-8 space-y-4 flex flex-col justify-evenly bg-[#d5dfd23f]"
             onSubmit={handleSubmit2}
           >
             <div className="input-wrapper">
               <label
                 htmlFor="role"
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-white text-sm font-bold mb-2"
               />
               <select
                 className="flex-shrink-0 z-10 inline-flex items-center w-full py-2 px-3
-                text-sm font-medium text-center text-gray-400 border
+                text-sm font-medium text-slate-500 text-center  border
                   rounded"
                 name="platform"
                 id="platform"
@@ -246,7 +246,7 @@ function Home() {
               <label htmlFor="enter-user" />
               <input
                 required
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+                className="shadow appearance-none border rounded w-full py-2 px-3 
                 leading-tight text-sm font-medium text-center 
                 focus:outline-none focus:shadow-outline"
                 id="user"
@@ -259,7 +259,7 @@ function Home() {
               </div>
               {/* [#f26419] orange */}
             <button
-                className="search-btn text-white bg-[#84d36c] hover:bg-[#293F14]
+                className="search-btn text-white bg-[#6186ad] hover:bg-[#4d6a88]
               font-medium
               rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
               type="submit"
@@ -272,7 +272,7 @@ function Home() {
 
 
         <div className="card-container grid gird-cols-2 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 lg:gap-x-6 gap-x-4 gap-y-4 lg:gap-y-2 lg:col-span-3 place-items-center">
-          <div className="w-9/12 h-full rounded-xl square aspect-w-1 aspect-h-1 bg-[#93a29b] shadow-lg p-4 ">
+          <div className="w-9/12 h-full rounded-xl square aspect-w-1 aspect-h-1  shadow-lg p-4 bg-[#a0a69f30]">
             <div className=" flex flex-col justify-center ">
             <div className="img-container p-4 flex justify-center h-full w-full md:pt-10 lg:pt-10 xl:pt-10 ">
               <img
@@ -281,7 +281,7 @@ function Home() {
               ></img>
             </div>
             <div className="palyer-info h-full flex-col w-full flex justify-center items-center space-y-3 font-maven p-2 ">
-            <p className=" p-3 block text-white text-base md:text-lg lg:text-2xl font-bold ">{`Player: ${userName}`}</p>
+            <p className=" p-3 block text-[#6186ad] text-base md:text-lg lg:text-2xl font-bold ">{`Player: ${userName}`}</p>
             <ul className="player-info-list text-white text-base md:text-base font-medium flex flex-col items-center">
               <li className="rounded px-2">{`Player Level: ${totalLevel}`}</li>
               <li className="rounded px-2">{`Ranked at number: ${playerRank}`}</li>
@@ -291,11 +291,11 @@ function Home() {
               </div> 
           </div>
           </div>
+          {/* bg-[#a0a69f] */}
 
-
-          <div className="legend-card w-9/12 h-full rounded-xl square aspect-w-1 aspect-h-1 bg-[#93a29b] shadow-lg font-maven p-4 ">
+          <div className="legend-card w-9/12 h-full rounded-xl square aspect-w-1 aspect-h-1  bg-[#a0a69f67] shadow-lg font-maven p-4 ">
             <div className="flex flex-col items-center" >
-            <p className="p-3 text-white text-base md:text-lg lg:text-2xl font-bold ">{`Current Legend: ${legendName}`}</p>
+            <p className="p-3 text-[#6186ad] text-base md:text-lg lg:text-2xl font-bold ">{`Current Legend: ${legendName}`}</p>
             <div className="img-container p-4 flex justify-center h-full w-full">
               <img className=" legend-card-img object-center object-contain rounded-xl " src={legendPic}></img>
               </div>
