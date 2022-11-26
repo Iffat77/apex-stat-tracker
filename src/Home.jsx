5ed477727f8f99d1525331a1223fdcdcfa8972a4
Iffat Hossain <iffathossain77@gmail.com>
2022-11-25 20:10:25 -0500

    adds temp server to bypass cors and removes proxy - temp fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
   const [data, setData] = useState();
   const [err, setErr] = useState();
   const [isErr, setIsErr] = useState(false)
-  const url = "v2/apex/standard/profile/";
+  const url = "https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/apex/standard/profile/";
 
   useEffect(() => {
     if (data) {
